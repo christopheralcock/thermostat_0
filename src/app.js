a42208469b5a631efeb1540715334d02f2f74bd9
Christopher Alcock <christopher.alcock@gmail.com>
2015-08-18 16:26:19 +0100

    fully functional using jquery

--- src/app.js
+++ src/app.js
@@ -8,42 +8,27 @@ $ (document).ready(function(){
     temperature.style.color = thermostat.colour();
   });
 
-  $("#power_saving_mode").show(function() {
+  $("#power_saving_mode").change(function() {
+    thermostat.powerSaveSwitch();
+  });
+
+
+  $("#increase").click(function() {
+    thermostat.increase();
+    temperature.innerHTML = thermostat.showTemperature();
+    temperature.style.color = thermostat.colour();
+  });
+
+  $("#decrease").click(function() {
+    thermostat.decrease();
+    temperature.innerHTML = thermostat.showTemperature();
+    temperature.style.color = thermostat.colour();
+  });
+
+  $("#reset").click(function() {
+    thermostat.resetTemperature();
     temperature.innerHTML = thermostat.showTemperature();
     temperature.style.color = thermostat.colour();
   });
 
 });
-
-
-
-
-// var temperature = document.getElementById('temperature');
-// temperature.innerHTML = thermostat.temperature();
-// temperature.style.color = thermostat.colour();
-
-// var power_saving_mode = document.getElementById('power_saving_mode');
-// power_saving_mode.onchange = function(){
-//   thermostat.powerSaveSwitch();
-// };
-
-// var increase = document.getElementById('increase');
-// increase.onclick = function() {
-//   thermostat.increase();
-//   temperature.innerHTML = thermostat.temperature();
-//   temperature.style.color = thermostat.colour();
-// };
-
-// var decrease = document.getElementById('decrease');
-// decrease.onclick = function(){
-//   thermostat.decrease();
-//   temperature.innerHTML = thermostat.temperature();
-//   temperature.style.color = thermostat.colour();
-// };
-
-// var reset = document.getElementById('reset');
-// reset.onclick = function(){
-//   thermostat.resetTemperature();
-//   temperature.innerHTML = thermostat.temperature();
-//   temperature.style.color = thermostat.colour();
-// };
