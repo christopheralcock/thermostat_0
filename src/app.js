afd27f248eaa21dc342e872d67d0570566bcb0fb
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-08-18 16:03:42 +0100

    started jquery

--- src/app.js
+++ src/app.js
@@ -1,44 +1,45 @@
 thermostat = new Thermostat();
-thermostat.powerSaveOn();
+temperaturehermostat.powerSaveOn();
 
-var temperature = document.getElementById('temperature');
-temperature.innerHTML = thermostat.temperature();
-temperature.style.color = thermostat.colour();
+$ (document).ready(function(){
+  
+  $("#temperature").show(function() {
+    temperature.innerHTML = thermostat.temperature();
+    // temperature.style.color = thermostat.colour();
+  });
 
-var power_saving_mode = document.getElementById('power_saving_mode');
-power_saving_mode.onchange = function(){
-  thermostat.powerSaveSwitch();
-};
-
-var increase = document.getElementById('increase');
-increase.onclick = function() {
-  thermostat.increase();
-  temperature.innerHTML = thermostat.temperature();
-  temperature.style.color = thermostat.colour();
-};
-
-var decrease = document.getElementById('decrease');
-decrease.onclick = function(){
-  thermostat.decrease();
-  temperature.innerHTML = thermostat.temperature();
-  temperature.style.color = thermostat.colour();
-};
-
-var reset = document.getElementById('reset');
-reset.onclick = function(){
-  thermostat.resetTemperature();
-  temperature.innerHTML = thermostat.temperature();
-  temperature.style.color = thermostat.colour();
-};
+})
 
 
-// var power_save_on = document.getElementById('power_save_on');
-// power_save_on.onclick = function(){
-//   thermostat.powerSaveOn();
+
+
+// var temperature = document.getElementById('temperature');
+// temperature.innerHTML = thermostat.temperature();
+// temperature.style.color = thermostat.colour();
+
+// var power_saving_mode = document.getElementById('power_saving_mode');
+// power_saving_mode.onchange = function(){
+//   thermostat.powerSaveSwitch();
 // };
-//
-//
-// var power_save_off = document.getElementById('power_save_off');
-// power_save_off.onclick = function(){
-//   thermostat.powerSaveOff();
+
+// var increase = document.getElementById('increase');
+// increase.onclick = function() {
+//   thermostat.increase();
+//   temperature.innerHTML = thermostat.temperature();
+//   temperature.style.color = thermostat.colour();
 // };
+
+// var decrease = document.getElementById('decrease');
+// decrease.onclick = function(){
+//   thermostat.decrease();
+//   temperature.innerHTML = thermostat.temperature();
+//   temperature.style.color = thermostat.colour();
+// };
+
+// var reset = document.getElementById('reset');
+// reset.onclick = function(){
+//   thermostat.resetTemperature();
+//   temperature.innerHTML = thermostat.temperature();
+//   temperature.style.color = thermostat.colour();
+// };
+
