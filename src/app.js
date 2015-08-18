9390c1c418a9cb2d9bfbba4d375d4000db66af06
Christopher Alcock <christopher.alcock@gmail.com>
2015-08-18 16:51:18 +0100

    added some really stupid animations

--- src/app.js
+++ src/app.js
@@ -19,16 +19,55 @@ $ (document).ready(function(){
     temperature.style.color = thermostat.colour();
   });
 
+  $("#increase").mouseenter(function() {
+    $(this).animate({
+        height: '+=20px'
+    });
+  });
+
+  $("#increase").mouseleave(function() {
+    $(this).animate({
+        height: '-=20px'
+    });
+  });
+
+
   $("#decrease").click(function() {
     thermostat.decrease();
     temperature.innerHTML = thermostat.showTemperature();
     temperature.style.color = thermostat.colour();
   });
 
+  $("#decrease").mouseenter(function() {
+    $(this).animate({
+        height: '+=20px'
+    });
+  });
+
+  $("#decrease").mouseleave(function() {
+    $(this).animate({
+        height: '-=20px'
+    });
+  });
+
+
+
   $("#reset").click(function() {
     thermostat.resetTemperature();
     temperature.innerHTML = thermostat.showTemperature();
     temperature.style.color = thermostat.colour();
+  });
+
+  $("#reset").mouseenter(function() {
+    $(this).animate({
+        height: '+=20px'
+    });
+  });
+
+  $("#reset").mouseleave(function() {
+    $(this).animate({
+        height: '-=20px'
+    });
   });
 
 });
